36b3947b6e579fd2b3f26e7a917649935fd4c5d6
Tesfa Tesfaye <tesfaget15@gmail.com>
2022-11-18 14:42:54 -0500

    secondCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState,useEffect } from 'react'
+import { useState,useEffect ,useRef} from 'react'
+import {ReactComponent as Tumbler}  from'./tumbler.svg'
+import {ReactComponent as Twitter} from './twitter.svg'
 import Quote from './Quote'
 import './App.css'
 
@@ -6,7 +8,8 @@ function App() {
   
  const[quote,setQuote]=useState({})
  const[quoteData, setQuoteData]=useState(()=>[])
-  
+ const [colorValue,setColorValue]=useState(randomNumber())
+  const ref=useRef(null)
   useEffect(()=>{
    async function fetchReq(){
     const res= await fetch("https://type.fit/api/quotes");
@@ -19,9 +22,9 @@ function App() {
 
  },[])
 
+
  useEffect(()=>{
-  console.log(quoteData)
-  if(quoteData.length>0){
+    if(quoteData.length>0){
     const randomValue=Math.floor(Math.random()*quoteData.length)
     setQuote(quoteData[randomValue])
    
@@ -29,21 +32,42 @@ function App() {
  
  },[quoteData])
 
+  function randomNumber(){
+    const randomNum=()=>Math.floor(Math.random()*128)
+    return (`rgb(${randomNum()},${randomNum()},${randomNum()}`)
+
+  }
 function nextQuote(){
-  const randomValue=Math.floor(Math.random*quoteData.length)
-  setQuote(quoteData[randomValue])
+  const randomValue=Math.floor(Math.random()*quoteData.length-1)
+  const value=quoteData[randomValue]
+  setQuote(value)
+  setColorValue(randomNumber())
+
+
 }
+ 
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor: colorValue}}>
       <div className='wrapper'>
+    
       <Quote
-       text={quote.text}
-       author={quote.author}
-      />
+      {...quote}
+       colorValue={colorValue}
+       ref={ref}
+       
+          />
+        <div className='author'  style={{color:colorValue}}><span >- {quote.author}</span></div>
+      <div className='buttonDiv'>
+        <div className='links'  style={{backgroundColor: colorValue}}> <Tumbler fill="white"  height={25}/></div>
+        <div className='links'  style={{backgroundColor: colorValue}}>  <Twitter fill="white" height={25} /></div>
+        <button className='NextQuote' style={{backgroundColor:colorValue}} onClick={nextQuote}>Next Quote</button>
+        </div>  
+        </div>
+        <div className='footer' > <span>by</span><a href="https://github.com/tesfatesfaye">   Tesfa</a></div>
       </div>
       
     
-      </div>
+      
   )
 }
 
--- src/Quote.jsx
+++ src/Quote.jsx
@@ -1,14 +1,24 @@
 import './App.css'
-
+import { useEffect ,useRef} from 'react'
+import {ReactComponent as Quotes} from "./quotes.svg"
 function Quote(props){
-
-
+    const ref=useRef(null)
+   
+    useEffect(()=>{
+        
+        ref.current.classList.add("invisible")
+        setTimeout(()=>{
+          
+            ref.current.classList.remove("invisible")
+        },200)
+       
+    },[props.text])
 
     return(
         <div className='quoteDiv'> 
         
-        <h1 className='hOne'>"{props.text}</h1>
-        
+        <h2 ref={ref}className='hOne' style={{color:props.colorValue}}><Quotes fill={props.colorValue} fillOpacity={1}stroke={props.colorValue} width={20} height={30} />  {props.text}</h2>
+       
         </div>
     )
 }
